459fbfdb6f5a25b75272b23b02b0a8cc952e5e62
Khaya <dlamkc@farmingdale.edu>
2025-02-13 20:49:41 -0500

    Made changes to the LoanCalculatorApplication to display the scene. Coding the Loan Calculator Pane.

---
+++ src/main/java/edu/farmingdale/dlamini_khayalamakhosi_loancalculator/LoanCalculatorApplication.java
@@ -0,0 +1,34 @@
+package edu.farmingdale.dlamini_khayalamakhosi_loancalculator;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoanCalculatorApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        /*FXMLLoader fxmlLoader = new FXMLLoader(LoanCalculatorApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();*/
+
+        Scene scene = new Scene(new GridPane(), 300, 150);
+        stage.setTitle("Loan Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/edu/farmingdale/dlamini_khayalamakhosi_loancalculator/LoanCalculatorPane.java
@@ -0,0 +1,20 @@
+package edu.farmingdale.dlamini_khayalamakhosi_loancalculator;
+import javafx.event.ActionEvent;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+public class LoanCalculatorPane extends GridPane
+{
+    private Label result;
+    private TextField input;
+
+    public LoanCalculatorPane()
+    {
+
+    }
+}
